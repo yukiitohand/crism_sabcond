d9909166801c8d3a4225a82bcaa7aea433161df0
Yuki Itoh <yukiitohand@gmail.com>
2019-04-24 12:20:41 -0400

    minor change in display

--- ancillary/interp_nan_column.m
+++ ancillary/interp_nan_column.m
@@ -4,6 +4,7 @@ rt_nan = sum(Xisnan,1)/L;
 for l=1:size(X,2)
     if any(Xisnan(:,l)) && rt_nan(l)<0.8
         good_idx = ~Xisnan(:,l);
-        X(Xisnan(:,l),l) = interp1(wv(good_idx),X(good_idx,l),wv(Xisnan(:,l)),'linear','extrap');
+        X(Xisnan(:,l),l) = interp1(wv(good_idx),X(good_idx,l),...
+            wv(Xisnan(:,l)),'linear','extrap');
     end
 end
