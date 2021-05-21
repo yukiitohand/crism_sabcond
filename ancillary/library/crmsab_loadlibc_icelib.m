52db41529f0fdf2ceff85053118f9a4b98031d9c
Yuki Itoh <yukiitohand@gmail.com>
2021-05-21 10:42:31 -0400

    dir_CACHE is updated

--- ancillary/library/crmsab_loadlibc_icelib.m
+++ ancillary/library/crmsab_loadlibc_icelib.m
@@ -1,15 +1,18 @@
 function [Aicelib,infoAicelib,valid_idx] = crmsab_loadlibc_icelib(opt,wabasename,c,bands_opt,wvc,varargin)
 
 global crism_env_vars
-localCRISM_PDSrootDir = crism_env_vars.localCRISM_PDSrootDir;
 
+dir_cache = crism_env_vars.dir_CACHE;
 overwrite = 0;
 cntRmvl = 0;
+
 if (rem(length(varargin),2)==1)
     error('Optional parameters should always go by pairs');
 else
     for i=1:2:(length(varargin)-1)
         switch upper(varargin{i})
+            case 'DIR_CACHE'
+                dir_cache = varargin{i+1};
             case 'CNTRMVL'
                 cntRmvl = varargin{i+1};
             case 'OVERWRITE'
@@ -20,8 +23,10 @@ else
     end
 end
 
-Alibdir = joinPath(localCRISM_PDSrootDir,'cache/WA/',wabasename);
-Alibcachefname = crmsab_constAicelibcachefname(wabasename,opt,bands_opt,c,cntRmvl);
+[wa_identfr] = crmsab_get_libWAIdentifier(wabasename);
+
+Alibdir = joinPath(dir_cache,'WA/',wa_identfr);
+Alibcachefname = crmsab_constAicelibcachefname(wa_identfr,opt,bands_opt,c,cntRmvl);
 Alibcachefilepath = joinPath(Alibdir,Alibcachefname);
 % Alibcachefilepath = joinPath(Alibdir,sprintf('Alib%s_%s_interp%s_b%s_c%03d.mat',libprefix,wabasename,optInterpid,bands_opt,c));
 if ~overwrite && exist(Alibcachefilepath,'file')
@@ -30,7 +35,7 @@ if ~overwrite && exist(Alibcachefilepath,'file')
         load(Alibcachefilepath,'infoAicelib','valid_idx');
     end
 else
-    [Aicelib,infoAicelib] = wrapper_crism_icelib(opt,wabasename,c);
+    [Aicelib,infoAicelib] = wrapper_crism_icelib(opt,wa_identfr,c);
     % select the band option
     bands = crmsab_genBands(bands_opt);
     Aicelib= Aicelib(bands,:) ; 
