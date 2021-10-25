4d9ea725e2173365d8b15fac85ee20231770b92d
Yuki Itoh <yukiitohand@gmail.com>
2021-10-25 14:41:22 -0400

    a bug is fixed

--- crism_sabcond_startup_addpath.m
+++ crism_sabcond_startup_addpath.m
@@ -15,6 +15,7 @@ crism_toolbox_dirname = 'crism_toolbox';
 crmcal_toolbox_dirname        = 'crism_calibration';
 crism_sabcond_toolbox_dirname = 'crism_sabcond';
 cntrmvl_toolbox_dirname       = 'continuumRemoval';
+suwab_toolbox_dirname         = 'suwab_toolbox';
 %-------------------------------------------------------------------------%
 pathCell = strsplit(path, pathsep);
 
@@ -39,7 +40,7 @@ envi_toolbox_dir = joinPath(toolbox_root_dir, envi_toolbox_dirname);
 
 if exist(envi_toolbox_dir,'dir')
     if ~check_path_exist(envi_toolbox_dir, pathCell)
-        run(joinPath(envi_toolbox_dir,'envi_startup_path'));
+        run(joinPath(envi_toolbox_dir,'envi_startup_addpath'));
     end
 else
     warning([ ...
@@ -53,7 +54,7 @@ pds3_toolbox_dir = joinPath(toolbox_root_dir, pds3_toolbox_dirname);
 
 if exist(pds3_toolbox_dir,'dir')
     if ~check_path_exist(pds3_toolbox_dir, pathCell)
-        run(joinPath(pds3_toolbox_dir,'pds3_startup_path'));
+        run(joinPath(pds3_toolbox_dir,'pds3_startup_addpath'));
     end
 else
     warning([ ...
@@ -67,12 +68,26 @@ crism_toolbox_dir = joinPath(toolbox_root_dir, crism_toolbox_dirname);
 
 if exist(crism_toolbox_dir,'dir')
     if ~check_path_exist(crism_toolbox_dir, pathCell)
-        run(joinPath(crism_toolbox_dir,'crism_startup_path'));
+        run(joinPath(crism_toolbox_dir,'crism_startup_addpath'));
     end
 else
     warning([ ...
         'crism_toolbox is not detected. Download from' '\n' ...
         '   https://github.com/yukiitohand/crism_toolbox/'
+        ]);
+end
+
+%% crism_calibration
+crmcal_toolbox_dir = joinPath(toolbox_root_dir, crmcal_toolbox_dirname);
+
+if exist(crmcal_toolbox_dir,'dir')
+    if ~check_path_exist(crmcal_toolbox_dir, pathCell)
+        run(joinPath(crmcal_toolbox_dir,'crism_calibration_startup_addpath'));
+    end
+else
+    warning([ ...
+        'crism_toolbox is not detected. Download from' '\n' ...
+        '   https://github.com/yukiitohand/crism_calibration/'
         ]);
 end
 
@@ -83,7 +98,7 @@ end
 
 %% crism_sabcond toolbox
 crism_sabcond_toolbox_dir = joinPath(toolbox_root_dir, crism_sabcond_toolbox_dirname);
-if ~check_path_exist(pds3_toolbox_dir, pathCell)
+if ~check_path_exist(crism_sabcond_toolbox_dir, pathCell)
     addpath( ...
         crism_sabcond_toolbox_dir                                  , ...
         joinPath(crism_sabcond_toolbox_dir,'legacy/')              , ...
